1d13439b4780d5b28fd3cf43f81ad795d781fa2b
“shisandong” <18511085166@163.com>
2026-01-27 16:11:00 +0800

    添加了战斗场景和UnitUI

---
+++ Assets/Scripts/Features/UI/UnitUI.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Features.Units.Core;
+
+namespace Features.UI
+{
+    public class UnitUI : MonoBehaviour
+    {
+        [Header("UI Components")] 
+        //方便测试，这里暂时先用Slider代替
+        [field:SerializeField] private Slider _hpSlider;
+
+        [field:SerializeField] private Image _fillImage;
+        [Header("Settings")]
+        [SerializeField] private Color _hpHighColor = Color.green;
+        
+        [SerializeField] private Color _hpLowColor = Color.red;
+        
+        private Unit _targetUnit;
+
+       
+
+        public void Initialize(Unit unit)
+        {
+            _targetUnit = unit;
+            _targetUnit.OnHpChanged += UpdateHealthBar;
+            UpdateHealthBar((float)unit.CurrentHp / unit.MaxHp);
+        }
+
+        /// <summary>
+        /// 更新角色血条
+        /// </summary>
+        /// <param name="hpPercent">Hp值百分比</param>
+        private void UpdateHealthBar(float hpPercent)
+        {
+            if (_hpSlider == null)
+                return;
+            
+            _hpSlider.value = hpPercent;
+            
+            if (_fillImage != null)
+            {
+                _fillImage.color = hpPercent > 0.5f ? _hpHighColor : _hpLowColor;
+            }
+        }
+        
+        private void Start()
+        {
+            if (_targetUnit == null)
+            {
+                var unit = GetComponentInParent<Unit>();
+                if (unit != null)
+                {
+                    Initialize(unit);
+                }
+                else
+                {
+                    Debug.LogError("UnitUI: No Unit found in parent.");
+                }
+            }
+        }
+        
+        private void OnDestroy()
+        {
+            // 取消订阅，防止报错
+            if (_targetUnit != null)
+            {
+                _targetUnit.OnHpChanged -= UpdateHealthBar;
+            }
+        }
+    }
+}
+
